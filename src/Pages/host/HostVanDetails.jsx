e4f4c9fbcc22fd23bb2c3cc8aaa99cb6036c92f9
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-03-27 01:01:33 +0200

    updated HostVandetails.jsx

--- src/Pages/host/HostVanDetails.jsx
+++ src/Pages/host/HostVanDetails.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
-export default function HostVanDetails() {
-  /**
-   * Challenge (not optional!): build the shared UI portion of the
-   * Host Van Detail page. This is
-   *
-   * Optional portion: also style it to look like the design.
-   *
-   * For now, get the data from a request to `/api/host/vans/:id`
-   * and display the van image, name, price, type
-   */
+export default function HostVanDetail() {
   const { id } = useParams();
   const [currentVan, setCurrentVan] = React.useState(null);
 
@@ -18,18 +9,30 @@ export default function HostVanDetails() {
     fetch(`/api/host/vans/${id}`)
       .then((res) => res.json())
       .then((data) => setCurrentVan(data.vans));
-  }, []);
+  }, [id]);
 
   if (!currentVan) {
     return <h1>Loading...</h1>;
   }
-
   return (
-    <div>
-      <img src={currentVan.imageUrl} width={150} />
-      <h2>{currentVan.name}</h2>
-      <p>{currentVan.price}</p>
-      <p>{currentVan.type}</p>
-    </div>
+    <section>
+      <Link to=".." relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+      </Link>
+
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+        {/*Your changes will go here*/}
+      </div>
+    </section>
   );
 }
