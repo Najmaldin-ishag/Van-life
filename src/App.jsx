750808d27fb8c4630715b2469ea32973581fe440
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-03-26 02:31:31 +0200

    change from absolute routes to relative routes

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VansDetails />} />
-          <Route element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
