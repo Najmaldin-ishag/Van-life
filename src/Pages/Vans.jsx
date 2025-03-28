2f94ad7c2eb51c020e1513fefb1d097c457b984f
Najmaldin-ishag <115111332+Najmaldin-ishag@users.noreply.github.com>
2025-03-28 02:10:44 +0200

    updates vans.jsx

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -1,23 +1,12 @@
 import React from "react";
-import "../server.js";
 import { Link, useSearchParams } from "react-router-dom";
-
-/**
- * {
- * id: "1",
- * name: "Modest Explorer",
- * price: 60,
- * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
- * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png",
- * type: "simple"
- * }
- */
 
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const typeFilter = searchParams.get("type");
-  console.log(typeFilter);
   const [vans, setVans] = React.useState([]);
+
+  const typeFilter = searchParams.get("type");
+
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
@@ -44,9 +33,42 @@ export default function Vans() {
     </div>
   ));
 
+  /**
+   * Challenge: change the Links to buttons and use the
+   * setSearchParams function to set the search params
+   * when the buttons are clicked. Keep all the classNames
+   * the same.
+   */
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          onClick={() => setSearchParams("?type=simple")}
+          className="van-type simple"
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => setSearchParams("?type=luxury")}
+          className="van-type luxury"
+        >
+          Luxury
+        </button>
+        <button
+          onClick={() => setSearchParams("?type=rugged")}
+          className="van-type rugged"
+        >
+          Rugged
+        </button>
+        <button
+          onClick={() => setSearchParams("")}
+          className="van-type clear-filters"
+        >
+          Clear filter
+        </button>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
